9d4f1ed37baf2d124027ffd10bf5122b86f1d2e2
Miłosz Klim <71337890+DAXPL@users.noreply.github.com>
2023-11-01 13:54:55 +0100

    Big Bang

---
+++ Ecosystem.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+sf::CircleShape shape(300.f);
+
+void Draw(sf::RenderWindow* window);
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+
+        Draw(&window);
+    }
+
+    return 0;
+}
+
+void Draw(sf::RenderWindow* window)
+{
+    window->clear();
+    window->draw(shape);
+    window->display();
+}
